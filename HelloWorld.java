21b5d4b88ec74d9ab9af4e574527a03f534dbcc8
monuprajapati <143016982+monuprajapati@users.noreply.github.com>
2025-01-16 21:10:17 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+// This is a my simple Java program to print "Hello, World!"
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World! Monu here");
+    }
+}
